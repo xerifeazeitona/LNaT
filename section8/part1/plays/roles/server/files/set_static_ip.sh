5077af9d76d1b050cbe87d49d764afe94c7c36d9
xerifeazeitona <juliano.amaral@gmail.com>
2020-11-13 16:09:23 -0500

    Added section8, part 1 (Connection Testing)

---
+++ section8/part1/plays/roles/server/files/set_static_ip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nmcli con mod System\ eth0 ipv4.method manual ipv4.address 10.0.1.10 ipv4.gateway 10.0.1.1 ipv4.dns 10.0.1.1
+nmcli con mod Wired\ connection\ 1 ipv4.method manual ipv4.address 10.0.1.20 ipv4.gateway 10.0.1.1 ipv4.dns 10.0.1.1
+systemctl restart network
+nmcli
